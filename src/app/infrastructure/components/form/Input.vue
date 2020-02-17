d1a09309bd889cec3949f301bca7fd4f6aeb7c90
Erandir <aefs12junior@gmail.com>
2020-02-16 21:20:14 -0300

    Added configuration to set value in dynamic model

--- src/app/infrastructure/components/form/Input.vue
+++ src/app/infrastructure/components/form/Input.vue
@@ -1,7 +1,7 @@
 <template>
   <q-input
     :ref="field.model"
-    v-model="form[field.model]"
+    v-model="model"
     :label="field.title"
     :color="field.color"
     :dark="field.darkMode"
@@ -11,7 +11,7 @@
 
 <script>
 export default {
-  name: 'CheckBox',
+  name: 'Input',
   props: {
     field: {
       type: Object,
@@ -20,6 +20,30 @@ export default {
     form: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    model: {
+      get () {
+        return this.isArray() ? this.form[this.field.model][this.field.position] : this.form[this.field.model]
+      },
+      set (value) {
+        if (this.isArray()) {
+          this.setValueInArray(value)
+
+          return 0
+        }
+
+        this.form[this.field.model] = value
+      }
+    }
+  },
+  methods: {
+    setValueInArray (value) {
+      this.$set(this.form[this.field.model], this.field.position, value)
+    },
+    isArray () {
+      return this.field.modelIsArray
     }
   }
 }
