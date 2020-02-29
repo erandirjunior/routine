cd361762a0e5ef03997d6c0a66ace347cd557e5e
Erandir <aefs12junior@gmail.com>
2020-02-28 22:06:48 -0300

    Improvement on layout

--- src/app/view/layouts/MainLayout.vue
+++ src/app/view/layouts/MainLayout.vue
@@ -1,14 +1,22 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="secundary-color">
+    <q-header elevated class="primary-bg-color">
       <q-toolbar>
         <q-toolbar-title>
-          Lista de Tarefas
+          Routine
         </q-toolbar-title>
+        <q-space/>
+        <q-btn
+          round
+          flat
+          icon="menu"
+          color="white"
+          stack
+        />
       </q-toolbar>
     </q-header>
 
-    <q-page-container>
+    <q-page-container class="primary-bg-color">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -24,8 +32,9 @@ export default {
 }
 </script>
 
-<style>
-  .secundary-color {
-    background-color: #E63636 !important;
-  }
+<style lang="sass">
+  .primary-bg-color
+    background-color: $bg-color-app !important
+  .secondary-bg-color
+    background-color: $bg-color-app-secondary !important
 </style>
