92e0f25a082c0c50e4f13faf8c88c95c55e44e2b
Erandir <aefs12junior@gmail.com>
2020-03-24 09:55:18 -0300

    Added new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,39 @@ const routes = [
     path: '/',
     component: () => import('../app/view/layouts/MainLayout'),
     children: [
-      { path: '', component: () => import('../app/view/pages/Index.vue') },
-      { path: '/tasks', component: () => import('../app/view/pages/Task.vue') },
-      { path: '/groups', component: () => import('../app/view/pages/Group.vue') }
+      {
+        path: '',
+        component: () => import('../app/view/pages/Index.vue')
+      },
+      {
+        path: '/tasks',
+        component: () => import('../app/view/pages/Tasks.vue')
+      },
+      {
+        path: '/groups',
+        component: () => import('../app/view/pages/Groups.vue')
+      },
+      {
+        path: '/group',
+        component: () => import('../app/view/pages/Group.vue')
+      },
+      {
+        path: '/group/:id',
+        component: () => import('../app/view/pages/Group.vue'),
+        props: true
+      },
+      {
+        path: '/task',
+        component: () => import('../app/view/pages/Task.vue')
+      },
+      {
+        path: '/about',
+        component: () => import('../app/view/pages/About.vue')
+      },
+      {
+        path: '/contact',
+        component: () => import('../app/view/pages/Contact.vue')
+      }
     ]
   }
 ]
@@ -15,7 +45,7 @@ const routes = [
 if (process.env.MODE !== 'ssr') {
   routes.push({
     path: '*',
-    component: () => import('pages/Error404.vue')
+    component: () => import('../app/view/pages/Error404.vue')
   })
 }
 
