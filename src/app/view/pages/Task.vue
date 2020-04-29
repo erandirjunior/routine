a57cd33ec3aa069f8a88adf79c20ae0a4c98ff2c
Erandir <aefs12junior@gmail.com>
2020-04-29 18:35:10 -0300

    Created task page

--- src/app/view/pages/Task.vue
+++ src/app/view/pages/Task.vue
@@ -1,6 +1,14 @@
 <template>
-  <q-page class="q-pa-sm">
-    <form-factory :fields="fields" :form="form" @action="action"/>
+  <q-page class="q-pa-xs">
+    <form-factory :fields="fields" :form="form" @formAction="action"/>
+    <q-btn
+      style="margin-top: 10px"
+      class="full-width"
+      color="teal-5"
+      label="SAVE"
+      v-if="showSaveButton"
+      @click="create"
+    />
   </q-page>
 </template>
 
@@ -9,6 +17,7 @@ import TaskBuilder from '../../infrastructure/builder/TaskBuilder'
 import FormFactory from '../../infrastructure/components/form/FormFactory'
 import handlerFormMixin from '../../infrastructure/mixins/handlerFormMixin'
 import TaskController from '../../application/controllers/TaskController'
+import GroupController from '../../application/controllers/GroupController'
 
 export default {
   name: 'PageIndex',
@@ -16,11 +25,17 @@ export default {
     return {
       fields: new TaskBuilder(),
       controller: new TaskController(),
+      groupController: new GroupController(),
+      step: 1,
       form: {
         title: '',
         activityTitle: '',
         activity: [],
-        check: [false]
+        check: [],
+        groupId: '',
+        startDate: '',
+        finalDate: '',
+        repeat: false
       }
     }
   },
@@ -31,15 +46,17 @@ export default {
     FormFactory
   },
   methods: {
-    action (emit) {
-      this[emit.action.action](emit)
+    showSaveButton () {
+      return this.form.activity.length > 0
     },
     add () {
-      this.form.activity.push(this.form.activityTitle)
-      this.form.check.push(false)
-      this.form.activityTitle = ''
-      const length = this.form.activity.length
-      this.fields.addActivityLine(length - 1)
+      if (this.form.activityTitle) {
+        this.form.activity.push(this.form.activityTitle)
+        this.form.check.push(false)
+        this.form.activityTitle = ''
+        const length = this.form.activity.length
+        this.fields.addActivityLine(length - 1)
+      }
     },
     remove (emit) {
       const position = emit.action.field.position
@@ -56,7 +73,26 @@ export default {
       this.form.activity.forEach((item, index) => {
         this.fields.addActivityLine(index)
       })
+    },
+    loadGroups () {
+      this.groupController.index()
+        .then(data => {
+          const items = data.map(item => {
+            return { label: item.name, value: item.id }
+          })
+
+          this.fields.addOptions(items)
+        })
     }
+  },
+  created () {
+    this.loadGroups()
   }
 }
 </script>
+
+<style>
+  .q-checkbox__bg {
+    border-radius: 50% !important;
+  }
+</style>
