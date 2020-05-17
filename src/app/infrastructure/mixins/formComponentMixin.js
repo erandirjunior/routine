2a4db7ba13d6fd1a7212b2482b284683c63cb22c
Erandir <aefs12junior@gmail.com>
2020-05-16 23:36:53 -0300

    Created mixin to handle form components and send information to controllers

---
+++ src/app/infrastructure/mixins/formComponentMixin.js
@@ -0,0 +1,51 @@
+export default {
+  props: {
+    field: {
+      type: Object,
+      required: true
+    },
+    form: {
+      type: Object,
+      required: true
+    },
+    validation: {
+      type: Object
+    }
+  },
+  computed: {
+    error () {
+      if (this.validation !== undefined) {
+        if (this.validation.form[this.field.model]) {
+          return this.validation.form[this.field.model].$error
+        }
+      }
+
+      return false
+    },
+    model: {
+      get () {
+        return this.isArray() ? this.form[this.field.model][this.field.position] : this.form[this.field.model]
+      },
+      set (value) {
+        if (this.isArray()) {
+          this.setValueInArray(value)
+
+          return 0
+        }
+
+        this.form[this.field.model] = value
+      }
+    }
+  },
+  methods: {
+    action (action) {
+      this.$emit('action', { action: action, value: this.form, field: this.field })
+    },
+    setValueInArray (value) {
+      this.$set(this.form[this.field.model], this.field.position, value)
+    },
+    isArray () {
+      return this.field.modelIsArray
+    }
+  }
+}
--- src/app/infrastructure/mixins/handlerFormMixin.js
+++ src/app/infrastructure/mixins/handlerFormMixin.js
@@ -1,30 +1,105 @@
+import alert from 'src/app/infrastructure/alert/Alert'
+
 export default {
   data () {
     return {
-      controller: ''
+      controllerCreate: '',
+      controllerUpdate: '',
+      controllerDelete: '',
+      failMessageCreate: '',
+      failMessageUpdate: '',
+      failMessageDelete: '',
+      successMessageCreate: '',
+      successMessageUpdate: '',
+      successMessageDelete: ''
     }
   },
-  method: {
-    beforeCreate () {},
-    beforeUpdate () {},
-    beforeDelete () {},
-    afterCreate () {},
-    afterUpdate () {},
-    aftereDelete () {},
+  validations: {
+  },
+  methods: {
     create () {
       this.beforeCreate()
-      this.controller.create(this.form)
-      this.afterCreate()
+
+      if (!this.enableValidationIfHasPropertyWasDefined('form')) {
+        this.controllerCreate.create(this.form)
+          .then(() => this.afterSuccessCreate())
+          .catch(() => this.afterFailCreate())
+      }
     },
     update () {
       this.beforeUpdate()
-      this.controller.update(this.form)
-      this.afterUpdate()
+
+      if (!this.enableValidationIfHasPropertyWasDefined('form')) {
+        this.controllerUpdate.update(this.form)
+          .then(() => this.afterSuccessUpdate())
+          .catch(() => this.afterFailUpdate())
+      }
     },
     delete () {
       this.beforeDelete()
-      this.controller.create(this.form)
-      this.aftereDelete()
+      this.controllerDelete.delete(this.form)
+        .then(() => this.afterSuccessDelete())
+        .catch(() => this.afterFailDelete())
+    },
+    action (emit) {
+      const action = typeof emit.action === 'string' ? emit.action : emit.action.action
+      this[action](emit)
+    },
+    beforeCreate () {},
+    beforeUpdate () {},
+    beforeDelete () {},
+    afterFailCreate () {
+      if (this.failMessageCreate) {
+        return this.showAlert(this.failMessageCreate, 'red', 'thumb_down')
+      }
+
+      this.showAlert('Fail to save data!', 'red', 'thumb_down')
+    },
+    afterFailUpdate () {
+      if (this.failMessageUpdate) {
+        return this.showAlert(this.failMessageUpdate, 'red', 'thumb_down')
+      }
+
+      this.showAlert('Fail to update data!', 'red', 'thumb_down')
+    },
+    afterFailDelete () {
+      if (this.failMessageDelete) {
+        return this.showAlert(this.failMessageDelete, 'red', 'thumb_down')
+      }
+
+      this.showAlert('Fail to delete data!', 'red', 'thumb_down')
+    },
+    afterSuccessCreate () {
+      if (this.successMessageCreate) {
+        return this.showAlert(this.successMessageCreate, 'green', 'thumb_up')
+      }
+
+      this.showAlert('Success to save data!', 'green', 'thumb_up')
+    },
+    afterSuccessUpdate () {
+      if (this.successMessageUpdate) {
+        return this.showAlert(this.successMessageUpdate, 'green', 'thumb_up')
+      }
+
+      this.showAlert('Success to update data!', 'green', 'thumb_up')
+    },
+    afterSuccessDelete () {
+      if (this.successMessageDelete) {
+        return this.showAlert(this.successMessageDelete, 'green', 'thumb_up')
+      }
+
+      this.showAlert('Success to delete data!', 'green', 'thumb_up')
+    },
+    showAlert (message, color, icon) {
+      alert(message, color, icon)
+    },
+    enableValidationIfHasPropertyWasDefined (property) {
+      if (this.$v.hasOwnProperty(property)) {
+        this.$v.form.$touch()
+        return this.$v.form.$error
+      }
+
+      return false
     }
   }
 }
