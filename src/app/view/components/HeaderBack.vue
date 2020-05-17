25aa80e8b4fef51b8412053890bab0ccec375d87
Erandir <aefs12junior@gmail.com>
2020-05-16 23:32:17 -0300

    Created header-back component

---
+++ src/app/view/components/HeaderBack.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-header elevated class="primary-bg-color">
+    <q-toolbar>
+      <q-toolbar-title>
+        <q-btn
+          round
+          flat
+          icon="keyboard_arrow_left"
+          color="white"
+          stack
+          to="/"
+        />
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBack'
+}
+</script>
+
+<style scoped>
+
+</style>
