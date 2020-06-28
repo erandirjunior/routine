a8fa999f1b626247dd36c1dc262231844aef35af
Erandir <aefs12junior@gmail.com>
2020-06-27 23:08:29 -0300

    Fixed paths icons

--- src/app/infrastructure/view/mixins/handlerActionMixin.js
+++
@@ -1,8 +0,0 @@
-export default {
-  methods: {
-    action (emit) {
-      const action = typeof emit.action === 'string' ? emit.action : emit.action.action
-      this[action](emit)
-    }
-  }
-}
--- src/app/infrastructure/view/mixins/handlerAlertMixin.js
+++
@@ -1,9 +0,0 @@
-import alert from 'src/app/infrastructure/components/alert/alert'
-
-export default {
-  methods: {
-    showAlert (message, color, icon) {
-      alert(message, color, icon)
-    }
-  }
-}
